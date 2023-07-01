5aae6d2d296880ba9e68523faa1cbd347e219b5d
nehchalm <114350987+nehchalm@users.noreply.github.com>
2023-07-01 11:15:28 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBi_ODgsO_Tx1QukCyrpqEZes0CmtVrw3c",
+  authDomain: "let-chat-web-app-d46cd.firebaseapp.com",
+  projectId: "let-chat-web-app-d46cd",
+  storageBucket: "let-chat-web-app-d46cd.appspot.com",
+  messagingSenderId: "1071560115743",
+  appId: "1:1071560115743:web:3c3e65711258940b71772a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
